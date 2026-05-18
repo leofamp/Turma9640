4a086fdd553c04b4d191d1773c7a0f528e03e0e2
leofamp <engleofamp@gmail.com>
2026-05-18 17:10:42 -0300

    testes javascript

---
+++ teste.js
@@ -0,0 +1,9 @@
+for (let tocha = 1; tocha<= 10 ; tocha++){
+    console.log("Tocha " + tocha + " acesa")
+}
+
+let tocha = 1
+while (tocha <= 10) {
+    console.log("Tocha " + tocha + " acesa")
+    tocha++
+}
