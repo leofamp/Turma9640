4ae98efe35163b08f6539ea98a24f763e7f2bcf1
leofamp <engleofamp@gmail.com>
2026-05-18 21:44:18 -0300

    Revisão aula01

---
+++ aula01/teste.js
@@ -0,0 +1,61 @@
+/*let nome = "Leo";
+let idade = 18;
+
+console.log(nome);
+console.log(idade);
+
+nome = "teto";
+idade = "12";
+
+console.log(nome);
+console.log(idade);
+
+
+for (let numero = 1; numero <=5; numero++){
+    console.log("Contagem: ", numero)
+}//para
+
+let numero = 1
+while (numero <= 5){
+    console.log("Contagem: ", numero)
+    numero++
+}//enquanto
+
+let idade = 60;
+
+if (idade >= 60 || idade <= 12){
+    console.log("É um idoso ou criança.");
+}
+  
+}else if (idade >= 12){ 
+    console.log("É um adolescente.");
+}else {
+    console.log("É uma criança")
+}
+
+
+let a = 10;
+let b = 5;
+
+console.log(a === b);//false
+console.log(a >= b);//true
+console.log(a <= b);//false
+console.log(a > 12 && b < 10);//false
+            //false e true
+console.log(a > 5 || b < 1);
+            //true ou //false
+console.log(a != b);//true
+console.log(!(a === b));//True
+
+*/
+
+function soma(n1, n2){
+    return n1 + n2
+}
+let somar = soma(10,30)
+console.log(somar)
+
+
+
+
+
--- teste.js
+++
@@ -1,9 +0,0 @@
-for (let tocha = 1; tocha<= 10 ; tocha++){
-    console.log("Tocha " + tocha + " acesa")
-}
-
-let tocha = 1
-while (tocha <= 10) {
-    console.log("Tocha " + tocha + " acesa")
-    tocha++
-}
