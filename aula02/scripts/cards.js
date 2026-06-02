d179f0486a99c8f17799c8e80a6d7ff4e89b91ce
leofamp <engleofamp@gmail.com>
2026-06-01 22:50:00 -0300

    alteraçoes feitas durante a aula

---
+++ aula02/scripts/cards.js
@@ -0,0 +1,19 @@
+class CardManager{
+    
+    flippedCards = new Set();
+
+    urlFactory;
+
+    constructor(factory){
+        this.urlFactory = factory;
+
+    }
+
+    gen(heroNumber){
+        let template = document.getElementById("cardTemplate")
+        let clone = template.content.cloneNode(true)
+
+        return clone
+    }
+    
+}
--- aula02/scripts/main.js
+++ aula02/scripts/main.js
@@ -1,6 +1,7 @@
 let menu = document.getElementById("menu");
 let select = document.getElementById("numCards");
 let start = document.getElementById("start");
+let boardDiv = document.getElementById("board");
 
 
 let nivel = 1;
@@ -13,7 +14,19 @@ for (let i = 4; i <=10 ; i+=2){
     op.value = n;
 
     op.textContent = `Nível ${nivel} - ${n} cartas`
+
     select.appendChild(op);
 
     nivel++;
 }
+
+start.addEventListener("click", ()=>{
+    menu.classList.add("hidden");
+    boardDiv.classList.remove("hidden");
+})
+
+start.click()
+
+
+
+
